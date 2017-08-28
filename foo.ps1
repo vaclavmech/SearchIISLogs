7dcce7fb8c509e1358c973407d0bc14524c3af40
vaclavmech <vaclav.mech@gmail.com>
2017-08-28 11:15:56 +0200

    2

--- foo.ps1
+++ foo.ps1
@@ -11,7 +11,11 @@ param(
     [Parameter(
         Position = 2
     )]
-    [string] $anotherText = ""
+    [string] $anotherText = "",
+    [Parameter(
+        Position = 3
+    )]
+    [bool] $formatted = $false
 )
 
 $source = @"
@@ -41,10 +45,44 @@ catch {
     # this is just to discard the exception :)
 }
 
-Write-Verbose "Processing $($filename.FullName).."
+Write-Verbose "Processing $($filename).."
 
-[string]$result += [MyClass]::Process($fileName.FullName, $userName, $anotherText)
+$result += [MyClass]::Process($fileName, $userName, $anotherText)
+# split and remove spaces
+$result = $result.Split("`n")
+$result = $result.Trim()
+# remove empty lines
+$result = $result |Where-Object {$_}
+
+if ($formatted) {
+    $formattedResult = @()
+    foreach ($item in $result) {
+        $item = $item.Split(" ")
+        $formattedResult += [PSCustomObject] @{
+            "date"              = $item[0]
+            "time"              = $item[1]
+            "s-ip"              = $item[2]
+            "cs-method"         = $item[3]
+            "cs-uri-stem"       = $item[4]
+            "cs-uri-query"      = $item[5]
+            "s-port"            = $item[6]
+            "cs-username"       = $item[7]
+            "c-ip"              = $item[8]
+            "cs(User-Agent)"    = $item[9]
+            "cs(Referer)"       = $item[10]
+            "sc-status"         = $item[11]
+            "sc-substatus"      = $item[12]
+            "sc-win32-status"   = $item[13]
+            "time-taken"        = $item[14]
+        }
+    }
+}
+
+if ($formatted) {
+    $result = $formattedResult
+}
+
 $result
 
 # example:
-# PS F:\> gci *.log| select fullname | %{.\foo.ps1 -fileName $_ -userName mechhvac -anotherText "Active" -Verbose} >> export.txt
+# PS F:\> gci c:\temp\*.log| select fullname | %{.\foo.ps1 -fileName $_.FullName -userName mechhvac -anotherText "Active" -Verbose} >> export.txt
